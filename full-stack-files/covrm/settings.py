0de4ddbe542156a0e19d309d696a2091421303d8
Sarvang Dave <32327152+sarvang00@users.noreply.github.com>
2021-05-15 15:24:48 +0530

    Addedn the guest user app

--- full-stack-files/covrm/settings.py
+++ full-stack-files/covrm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'guests.apps.GuestsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
