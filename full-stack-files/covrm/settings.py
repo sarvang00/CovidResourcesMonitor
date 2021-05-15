91bb25b8935b45f65ea8514a4244430ae2cdc63d
Sarvang Dave <32327152+sarvang00@users.noreply.github.com>
2021-05-16 00:06:18 +0530

    Updated Database properties

--- full-stack-files/covrm/settings.py
+++ full-stack-files/covrm/settings.py
@@ -66,8 +66,11 @@ WSGI_APPLICATION = 'covrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'covrmdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgresRoot@123',
+        'HOST': 'localhost'
     }
 }
 
