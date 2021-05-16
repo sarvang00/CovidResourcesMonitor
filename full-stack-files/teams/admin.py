9ea3fd05dc67f03493cc8d8cb2bbdcbb1d0360b7
Sarvang Dave <32327152+sarvang00@users.noreply.github.com>
2021-05-16 15:12:32 +0530

    Added custom functionality and style to admin dash

--- full-stack-files/teams/admin.py
+++ full-stack-files/teams/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 
-from .models import Location, Resources, Lead, Availability
+from .models import Location, Resource, Lead, Availability
 
-admin.site.register(Location)
-admin.site.register(Resources)
-admin.site.register(Lead)
-admin.site.register(Availability)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lead_name', 'contact_num', 'location', 'email', 'lead_type')
+    list_display_links = ('id', 'lead_name')
+    list_filter = ('location', 'lead_type')
+    search_fields = ('location', 'lead_type')
+    list_per_page = 25
+
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lead', 'resource_type', 'available_count')
+    list_display_links = ('id', 'lead')
+    list_filter = ('resource_type',)
+    search_fields = ('lead', 'resource_type')
+    list_per_page = 25
+
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'sub_category')
+    list_filter = ('category',)
+    search_fields = ('category', 'sub_category')
+    list_per_page = 25
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'state', 'region')
+    list_filter = ('state',)
+    search_fields = ('state', 'region')
+    list_per_page = 25
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Lead, LeadAdmin)
+admin.site.register(Availability, AvailabilityAdmin)
