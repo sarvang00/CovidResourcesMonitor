fd9ddcfe8616c7ee134ad02c87de3d46fde61753
Sarvang Dave <32327152+sarvang00@users.noreply.github.com>
2021-05-23 17:30:48 +0530

    Guest dashboard made dynamic

--- full-stack-files/guests/views.py
+++ full-stack-files/guests/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
-from teams.models import Location, Resource, LeadType
+from teams.models import Location, Resource, LeadType, Lead, Availability
 from .models import ProposedAvailability, ProposedLead
 from datetime import datetime
 
@@ -14,7 +14,180 @@ def index(request):
         return redirect('dashboard')
 
 def dashboard(request):
-    return render(request, 'guests/guest-dashboard.html')
+    if request.method == "POST":
+        # Filtering states
+        locations = Location.objects.all()
+
+        states = list()
+    
+        for location in locations:
+            states.append(location.state)
+        
+        states = list(set(states))
+
+        location_ids = list()
+
+        # Getting and making sense of POST requests
+        state = request.POST['selectedState']
+
+        try:
+            region = request.POST['selectedRegion']
+        except:
+            region = "all"
+        
+        category = request.POST['selectCategory']
+
+        selected_lead_type = request.POST['selectLeadType']
+        lead_type = ""
+
+        if selected_lead_type == "all":
+            # it is all
+            lead_type = "all"
+        else:
+            # need to filter
+            lead_type = LeadType.objects.filter(type=selected_lead_type).all()[0].id
+
+        print("State: ", state, " region: ", region, " catrgory: ", category, " lead type: ", lead_type)
+
+        # Getting the right location
+        if state=="all":
+            locations = Location.objects.all()
+        else:
+            locations = Location.objects.filter(state=state).filter(region=region).all()
+            
+            for selected_loc in locations:
+                location_ids.append(selected_loc.id)
+        
+               
+        # Filtering categories and resources
+        resources = Resource.objects.all()
+
+        categories = list()
+
+        for resource in resources:
+            categories.append(resource.category)
+        
+        categories = list(set(categories))
+
+        resources = Resource.objects.filter(category=category).all()
+
+        # Making a list of resources ids
+        resources_ids = list()
+        for selectedResource in resources:
+            resources_ids.append(selectedResource.id)
+
+        # Making a list of sub-categories
+        sub_cats = list()
+
+        for resource in resources:
+            sub_cats.append(resource.sub_category)
+        
+        sub_cats = list(set(sub_cats))
+
+        # Lead types
+        lead_types = LeadType.objects.all()
+
+        # Filtering leads and availabilities
+        if state=="all":
+            # for all locations
+            if lead_type=="all":
+                # only need to filter availabilities
+                leads = Lead.objects.all()
+            else:
+                # check for particular lead type
+                leads = Lead.objects.filter(lead_type__id=lead_type).all()
+        else:
+            # for a particular location
+            if lead_type=="all":
+                # only need to filter availabilities
+                leads = Lead.objects.filter(location__id__in=location_ids).all()
+            else:
+                # check for particular lead type
+                leads = Lead.objects.filter(location__id__in=location_ids).filter(lead_type__id=lead_type).all()
+        
+        # Getting lead ids
+        lead_ids = list()
+        for selectedLead in leads:
+            lead_ids.append(selectedLead.id)
+
+        # Filtering availabilities via leads and resources
+        availabilties = Availability.objects.filter(lead__id__in=lead_ids).filter(resource_type__id__in=resources_ids).all()
+
+        context= {
+            'locations' : locations,
+            'resources': resources,
+            'sub_categories': sub_cats,
+            'leads': leads,
+            'availabilies': availabilties,
+            'states': states,
+            'categories': categories,
+            'lead_types': lead_types
+        }
+
+    else:
+        # Filtering all locations
+        locations = Location.objects.all()
+        
+        # Filtering resources and category
+        resources = Resource.objects.all()
+        
+        categories = list()
+
+        for resource in resources:
+            categories.append(resource.category)
+        
+        categories = list(set(categories))
+
+        resources = Resource.objects.filter(category=categories[0]).all()
+
+        sub_cats = list()
+
+        for resource in resources:
+            sub_cats.append(resource.sub_category)
+        
+        sub_cats = list(set(sub_cats))
+
+        # Filtering states
+        states = list()
+    
+        for location in locations:
+            states.append(location.state)
+        
+        states = list(set(states))
+
+        # Filtering lead ids
+        location_ids = list()
+        for location in locations:
+            location_ids.append(location.id)
+
+        leads = Lead.objects.filter(location__id__in=location_ids)
+        lead_ids = list()
+        for lead in leads:
+            lead_ids.append(lead.id)
+
+        # Filtering resource ids
+        resource_ids = list()
+        for resource in resources:
+            resource_ids.append(resource.id)
+
+        # Availabilities
+        availabilties = Availability.objects.filter(lead__id__in=lead_ids).filter(resource_type__id__in=resource_ids).all()
+
+        # Lead types
+        lead_types = LeadType.objects.all()
+        
+        context= {
+            'locations' : locations,
+            'resources': resources,
+            'sub_categories': sub_cats,
+            'leads': leads,
+            'availabilies': availabilties,
+            'states': states,
+            'categories': categories,
+            'lead_types': lead_types
+        }
+
+    return render(request, 'guests/guest-dashboard.html', context=context)
 
 def contribute(request):
     if request.method == "POST":
