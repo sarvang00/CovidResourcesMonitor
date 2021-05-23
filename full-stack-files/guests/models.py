4cb7b5eac3eff5e328143634ef765a3cfccea95c
Sarvang Dave <32327152+sarvang00@users.noreply.github.com>
2021-05-23 13:23:57 +0530

    Updated proposed lead model

--- full-stack-files/guests/models.py
+++ full-stack-files/guests/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from teams.models import Location, Resource
+from teams.models import Location, Resource, LeadType
 
 class ProposedLead(models.Model):
     lead_name = models.CharField(max_length=100)
@@ -9,7 +9,7 @@ class ProposedLead(models.Model):
     full_address = models.TextField(blank=True, null=True)
     gmaps_url = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    lead_type = models.CharField(max_length=100)
+    lead_type = models.ForeignKey(LeadType, on_delete=models.DO_NOTHING, related_name='leadtype')
     source = models.BooleanField()
     comments = models.TextField(blank=True, null=True)
     last_updated = models.DateTimeField(default=datetime.now)
